b9b5795d63658fe850e0b8e31d3b65e1459236bd
Reda El Marzouki <relmarzouki@gmail.com>
2023-10-05 08:57:09 -0400

    feat: add greeting

--- number_guess.sh
+++ number_guess.sh
@@ -3,18 +3,31 @@
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 NUMBER=$(( $RANDOM % 1000 + 1 ))
+echo $NUMBER
 
 echo "Enter your username:"
 read USERNAME
 
 USERNAME_ID=$($PSQL "select user_id from users where username='$USERNAME';")
-echo $USERNAME_ID
 
-# if [[ -z $USERNAME_ID ]]
-# then
-#     echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
-# else
-#     # get username, games_played(count) and best_game
-#     GUESSES_RESULTS=$($PSQL "select u.username, MIN(g.best_game), COUNT(g.best_game) from users as u inner join games as g using(user_id) where u.user_id=$USERNAME_ID;")
-#     echo "$GUESSES_RESULTS"
-# fi
+if [[ -z $USERNAME_ID ]]
+then
+    SET_NEW_USERNAME=$($PSQL "insert into users(username) values('$USERNAME');")
+    echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
+else
+    # get username, games_played(count) and best_game
+    NAME=$($PSQL "select username from users where user_id=$USERNAME_ID;")
+    GUESSES_RESULTS=$($PSQL "select MIN(best_game), COUNT(best_game) from users as u inner join games as g using(user_id) where u.user_id=$USERNAME_ID;")
+    echo "$GUESSES_RESULTS" | while IFS="|" read BEST_GAME GAME_PLAYED
+    do
+        echo -e "\nWelcome back, $NAME! You have played $GAME_PLAYED games, and your best game took $BEST_GAME guesses."
+    done
+fi
+
+echo -e "\nGuess the secret number between 1 and 1000:"
+read GUESS
+
+until [[ $GUESS == $NUMBER ]]
+do
+    echo got it
+done
